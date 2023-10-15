b211370e234d35867ccba4dba25512fedf4d532a
MohanedB <mohanedbouzaidi@gmail.com>
2023-10-15 19:22:22 -0400

    Tried internationalizatyion, but its too early for it, sonothing in this commit

--- responsiveportfolio/src/App.js
+++ responsiveportfolio/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
   return (
     <div className="App">
+     
+     
+      
       <header className="App-header">
+      
        Portfolio
+
+    
       </header>
     </div>
   );
-}
+} 
+
 
 export default App;
