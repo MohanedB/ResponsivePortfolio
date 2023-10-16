7f7c66a3bf6f0a3c49e32b9bbeb7bf3d0cb04df3
MohanedB <mohanedbouzaidi@gmail.com>
2023-10-15 20:55:10 -0400

    Almost fnished the navbar today

--- responsiveportfolio/src/App.js
+++ responsiveportfolio/src/App.js
@@ -1,21 +1,43 @@
 import './App.css';
+import styled, {ThemeProvider} from "styled-components";
+import { darkTheme } from './utils/Theme';
+import NavBar from "./components/Navbar/Navbar";
+import Hero from './components/HeroSection/Hero';
+import Skills from './components/Skills/Skills';
+import Education from './components/Education/Education';
+import { BrowserRouter as Router} from "react-router-dom";
 
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%,
+  overflow-x: hidden;
+`
 
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 
 function App() {
-
   return (
-    <div className="App">
-     
-     
+    <ThemeProvider theme={darkTheme}>
+    <Router>
+      <NavBar></NavBar>
+      <Body>
+        <Hero/>
+        <Wrapper>
+        <Skills/>
+        </Wrapper>
+        <Education/>
+     </Body>
+   </Router>
       
-      <header className="App-header">
-      
-       Portfolio
+     
+    </ThemeProvider>
 
-    
-      </header>
-    </div>
+   
   );
 } 
 
---
+++ responsiveportfolio/src/data/const.js
@@ -0,0 +1,14 @@
+export const Bio = {
+    name: "Mohaned Bouzaidi",
+    roles: [
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges with a passion for learning. I am dedicated to delivering high-quality results with a positive attitude and a great mindset and I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/MohanedB",
+    resume:
+      "https://drive.google.com/drive/my-drive",
+    linkedin: "https://www.linkedin.com/in/mohaned-bouzaidi-a7390724a/",
+  };
+  
