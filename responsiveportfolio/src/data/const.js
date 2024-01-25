2e6fa6ca2410aa4ce8be40389c491a8ad0b2433f
Mohaned <mohanedbouzaidi@gmail.com>
2024-01-25 17:16:49 -0500

    Update const.js

--- responsiveportfolio/src/data/const.js
+++ responsiveportfolio/src/data/const.js
@@ -118,7 +118,7 @@ export const Bio = {
         {
           name: "Postman",
           image:
-            "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+            "https://tse3.mm.bing.net/th/id/OIP.ev0hMHHlKjpQ19nJ23TSmwHaGq?rs=1&pid=ImgDetMain",
         },
         {
           name: "Figma",
