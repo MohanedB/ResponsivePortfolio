2735375d4543eb64c870c39798b26b4f13812195
Mohaned <mohanedbouzaidi@gmail.com>
2024-02-01 10:36:16 -0500

    trying to make the redirection work again
    trying to make the redirection to the google drive where the resume is work again

--- responsiveportfolio/src/components/Hero/Hero.js
+++ responsiveportfolio/src/components/Hero/Hero.js
@@ -243,7 +243,7 @@ const Hero = () => {
               </Span>
             </TextLoop>
             <SubTitle>{t('description')}</SubTitle>
-            <ResumeButton href={t('resumer')} target='display'>{t('resume')}</ResumeButton>
+            <ResumeButton href={t('resume')} target='display'>{t('resumer')}</ResumeButton>
           </HeroLeftContainer>
 
           <HeroRightContainer id="Right">
